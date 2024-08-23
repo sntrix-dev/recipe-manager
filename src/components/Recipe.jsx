1e6076e884a1fbe5396f7331c88680fe35252327
sntrix-dev <suryakrishnan2002@gmail.com>
2024-08-23 19:59:03 +0530

    UI improvised

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -22,19 +22,11 @@ const Recipe = ({
         isView ? "max-h-[85vh] overflow-auto" : ""
       }`}
     >
-      {!isView && (
-        <div className="absolute top-4 right-4 flex items-center gap-4">
-          <button className="p-1" onClick={onEdit}>
-            <EditIcon />
-          </button>
-          <button className="p-1" onClick={onDelete}>
-            <DeleteIcon />
-          </button>
-        </div>
-      )}
-      <h2 className="text-4xl font-bold text-primary mb-6">{name}</h2>
+      <h2 className="text-2xl lg:text-4xl font-bold text-primary mb-6">
+        {name}
+      </h2>
       <div>
-        <h3 className="text-xl font-semibold">Ingredients</h3>
+        <h3 className="text-lg lg:text-xl font-semibold">Ingredients</h3>
         <ul className="list-disc ml-4 mt-4">
           {(isView ? ingredients : ingredients.slice(0, 3)).map(
             (ingredient, index) => (
@@ -52,7 +44,7 @@ const Recipe = ({
         </ul>
       </div>
       <div className="mt-6">
-        <h3 className="text-xl font-semibold">Cooking Steps</h3>
+        <h3 className="text-lg lg:text-xl font-semibold">Cooking Steps</h3>
         <div className="mt-4">
           {(isView ? steps : steps.slice(0, 2)).map((step, index) => (
             <div key={index} className="flex gap-2">
@@ -69,15 +61,25 @@ const Recipe = ({
             </button>
           )}
           {!isView && (
-            <Button
-              size="lg"
-              fullWidth
-              theme="primary"
-              className=" mt-4"
-              onClick={handleView}
-            >
-              View Complete Recipe
-            </Button>
+            <div className="flex items-center  gap-10">
+              <Button
+                size="lg"
+                fullWidth
+                theme="primary"
+                className=" mt-4"
+                onClick={handleView}
+              >
+                View Complete Recipe
+              </Button>
+              <div className=" flex items-center justify-end gap-4 mt-4">
+                <button className="p-1 text-primary" onClick={onEdit}>
+                  <EditIcon />
+                </button>
+                <button className="p-1 text-primary" onClick={onDelete}>
+                  <DeleteIcon />
+                </button>
+              </div>
+            </div>
           )}
         </div>
       </div>
--- src/design/components/Modal.jsx
+++ src/design/components/Modal.jsx
@@ -3,7 +3,7 @@ import { CloseIcon } from "../icons";
 const Modal = ({ open, onClose, children }) => {
   return (
     <div
-      className={`w-full h-screen bg-dark/25 z-50 fixed top-0 left-0 flex items-center justify-center transition-all duration-300 ease-in-out ${
+      className={`w-full h-screen bg-dark/25 z-50 fixed top-0 left-0 flex items-center justify-center transition-all duration-300 ease-in-out px-4 ${
         open
           ? "opacity-100 pointer-events-auto"
           : "opacity-0 pointer-events-none"
@@ -12,9 +12,9 @@ const Modal = ({ open, onClose, children }) => {
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className="bg-light shadow-sm rounded-md py-6 px-10 min-w-[17rem] lg:w-1/2 relative"
+        className="bg-light shadow-sm rounded-md px-4 py-4 lg:py-6 lg:px-10 min-w-[17rem] lg:w-1/2 relative"
       >
-        <button className="absolute right-4 top-4" onClick={onClose}>
+        <button className="absolute right-4 top-4 z-50" onClick={onClose}>
           <CloseIcon size={24} />
         </button>
         {children}
